2e4498fabbf78c8864b872728d36f488d33c026e
Tyler Wozniak <ttw6741@rit.edu>
2014-02-25 15:22:30 -0500

    Working on stubbing out classes.

---
+++ Scripts/ability.d
@@ -0,0 +1,30 @@
+﻿module Scripts.actions;
+
+enum TargetType
+{
+	EnemyUnit,
+	AlliedUnit,
+	Ground
+}
+
+enum TargetArea
+{
+	Point,
+	Line,
+	Square,
+	Circle,
+}
+
+class Ability
+{
+private:
+	TargetType targetType;
+	TargetArea targetArea;
+	int range;
+	int radius; // Different uses for different Areas
+public:
+	this ()
+	{
+		// Constructor
+	}
+}
