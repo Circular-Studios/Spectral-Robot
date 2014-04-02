c29a96dcc53b99a61a7174d4e5c5747bb49a1dfc
Daniel Jost <torpedojost@gmail.com>
2014-04-01 20:53:43 -0400

    Can read in abilities from yaml.

--- Scripts/ability.d
+++ Scripts/ability.d
@@ -1,45 +1,55 @@
 ﻿module ability;
-import core;
+import core, utility;
 
 
 enum TargetType
 {
 	EnemyUnit,
 	AlliedUnit,
-	Ground
+	Ground,
 }
 
 enum TargetArea
 {
-	Point,
+	Single,
 	Line,
 	Square,
-	Circle,
+	Radial,
 }
 
 shared class Ability
 {
 private:
-	static uint nextID;
-
+	static uint nextID = 0;
+	string _name;
 	TargetType _targetType;
 	TargetArea _targetArea;
+	int _damage;
 	int _range;
-	int _radius; // Different uses for different Areas
 	int _cooldown;
+	
 public:
 	immutable uint ID;
-
+	mixin( Property!_name );
 	mixin( Property!_targetType );
 	mixin( Property!_targetArea );
+	mixin( Property!_damage );
 	mixin( Property!_range );
-	mixin( Property!_radius );
 	mixin( Property!_cooldown );
-
+	
 	this ()
 	{
-		// Constructor
 		ID = nextID;
 		nextID++;
+	}
+	
+	void init( string name, TargetType ttype, TargetArea tarea, int damage, int range, int cooldown )
+	{
+		_name = name;
+		_targetType = ttype;
+		_targetArea = tarea;
+		_damage = damage;
+		_range = range;
+		_cooldown = cooldown;
 	}
 }
--- Scripts/controller.d
+++ Scripts/controller.d
@@ -2,78 +2,96 @@
 import unit, ability, grid;
 import core, utility;
 import yaml;
-import std.path;
+import std.path, std.conv;
 
 final shared class Controller
 {
 public:
-	Action[] lastTurn; //Gets cleared after a turn
-	Action[] currentTurn; //Gets populated as the user makes actions
-	GameObjectCollection gameObjects; //Abstract this to a GameState class or something? Idk yet
-	Ability[string] abilities; //The instantiated units for this instance of the game
+	Action[] lastTurn; // Gets cleared after a turn
+	Action[] currentTurn; // Gets populated as the user makes actions
+	GameObjectCollection gameObjects; // Abstract this to a GameState class or something? Idk yet
+	Ability[string] abilities; // The instantiated units for this instance of the game
 	
 	this()
 	{
 		gameObjects = new shared GameObjectCollection();
-		// So we'll first load all the objects
+		
+		// first load all the objects
 		gameObjects.loadObjects("Base");
 		
+		// create the grid
 		auto grid = new shared Grid();
 		grid.transform.position.z = -50;
 		grid.transform.updateMatrix();
 		gameObjects["Grid"] = grid;
 		
+		// load the game
 		loadAbilities();
 		loadUnits();
 		loadLevel();
 	}
-
 	
+	/// Load and create abilities from yaml
 	void loadAbilities()
 	{
-		//Add the ability to the Ability array by loading it from yaml, just like in loadUnits
 		foreach( abilityNode; loadYamlDocuments( buildNormalizedPath( FilePath.Resources.Objects, "Abilities" ) ) )
 		{
+			// setup variables
+			string ttype, tarea;
+			TargetType targetType;
+			TargetArea targetArea;
+			int damage, range, cooldown;
+			
+			// get the variables from the yaml node
 			string name = abilityNode["Name"].as!string;
-			abilities[name] = new shared Ability();
+			if( Config.tryGet( "TargetType", ttype, abilityNode ) )
+			{
+				targetType = to!TargetType(ttype);
+			}
+			if( Config.tryGet( "TargetArea", tarea, abilityNode ) )
+			{
+				targetArea = to!TargetArea(tarea);
+			}
+			Config.tryGet( "Damage", damage, abilityNode );
+			Config.tryGet( "Range", range, abilityNode );
+			Config.tryGet( "Cooldown", cooldown, abilityNode );
+			
+			// initialize the ability
+			abilities[ name ] = new shared Ability();
+			abilities[ name ].init( name, targetType, targetArea, damage, range, cooldown );
 		}
 	}
 	
+	/// Load and create units from yaml
 	void loadUnits()
 	{
 		// So we are going to parse the Units folder for the unit files
 		// For those, we'll get the Name of the node, which will be how we call into the gameObjects
-		
 		foreach( unitNode; loadYamlDocuments( buildNormalizedPath( FilePath.Resources.Objects, "Units" ) ) )
 		{
+			// instantiate the prefab of a unit
 			string[shared GameObject] parents;
 			string[][shared GameObject] children;
-
-			auto unit = cast(shared Unit)Prefabs[ unitNode["InstanceOf"].as!string ].createInstance( parents, children );
-			unit.name = unitNode["Name"].as!string;
+			auto unit = cast(shared Unit)Prefabs[ unitNode[ "InstanceOf" ].as!string ].createInstance( parents, children );
 			
-			//Then for each variable, accessed by unitNode["varname"] or better off, a tryGet
-			//Set the values
+			// get the variables from the yaml node
 			int posX, posY, hp, sp, at, df = 0;
 			string ability;
-			shared Ability melee, ranged;
+			unit.name = unitNode["Name"].as!string;
 			Config.tryGet( "PosX", posX, unitNode );
 			Config.tryGet( "PosY", posY, unitNode );
 			Config.tryGet( "HP", hp, unitNode );
 			Config.tryGet( "Speed", sp, unitNode );
 			Config.tryGet( "Attack", at, unitNode );
 			Config.tryGet( "Defense", df, unitNode );
-			if( Config.tryGet( "MeleeAttack", ability, unitNode ) )
-				melee = abilities[ ability ];
-			if( Config.tryGet( "RangedAttack", ability, unitNode ) )
-				ranged = abilities[ ability ];
 			
-			unit.init(posX, posY, hp, sp, at, df, melee, ranged, [ melee, ranged ] );
-			
-			gameObjects[unit.name] = unit;
+			// initialize the unit and add it to the GameObjectCollection
+			unit.init( posX, posY, hp, sp, at, df, [] );
+			gameObjects[ unit.name ] = unit;
 		}
 	}
 	
+	/// Load and create levels from yaml
 	void loadLevel()
 	{
 		
