0afcc8dd72d170b66e4f35aaa5f43438349aeac8
Daniel Jost <torpedojost@gmail.com>
2014-04-04 23:00:20 -0400

    Added deltaTime to camera.

--- Scripts/camera.d
+++ Scripts/camera.d
@@ -1,5 +1,6 @@
 ﻿module camera;
 import core, utility;
+import std.algorithm;
 
 /// Temporary class to move the camera around
 shared class Camera : GameObject
@@ -10,33 +11,34 @@ shared class Camera : GameObject
 	
 	override void onUpdate()
 	{	
+		logInfo( max( Time.deltaTime.total!"msecs"(), 1 ) );
 		// 4-directional movement
 		if( Input.getState( "LookUp" ) )
 		{
-			this.transform.position.z -= 0.05;
+			this.transform.position.z -= 0.15 * max( Time.deltaTime.total!"msecs"(), 1 );
 		}
 		else if( Input.getState( "LookDown" ) )
 		{
-			this.transform.position.z += 0.05;
+			this.transform.position.z += 0.15 * max( Time.deltaTime.total!"msecs"(), 1 );
 		}
 		
 		if( Input.getState( "LookLeft" ) )
 		{
-			this.transform.position.x -= 0.05;
+			this.transform.position.x -= 0.15 * max( Time.deltaTime.total!"msecs"(), 1 );
 		}
 		else if( Input.getState( "LookRight" ) )
 		{
-			this.transform.position.x += 0.05;
+			this.transform.position.x += 0.15 * max( Time.deltaTime.total!"msecs"(), 1 );
 		}
 
 		// change distance from floor
 		if( Input.getState( "ZoomUp" ) )
 		{
-			this.transform.position.y -= 0.05;
+			this.transform.position.y -= 0.15 * max( Time.deltaTime.total!"msecs"(), 1 );
 		}
 		else if( Input.getState( "ZoomDown" ) )
 		{
-			this.transform.position.y += 0.05;
+			this.transform.position.y += 0.15 * max( Time.deltaTime.total!"msecs"(), 1 );
 		}
 	}
 }
