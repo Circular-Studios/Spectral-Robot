f25a9e328547ec1588300aa11edbce59c87d0b43
Daniel Jost <torpedojost@gmail.com>
2014-04-06 11:35:22 -0400

    Changed camera movement to arrow keys.

--- Scripts/camera.d
+++ Scripts/camera.d
@@ -2,42 +2,40 @@
 import core, utility;
 import std.algorithm;
 
-/// Temporary class to move the camera around
+/// Camera movement around the scene
 shared class Camera : GameObject
 {
-	this()
-	{
-	}
+	float moveSpeed = 0.15;
 	
 	override void onUpdate()
 	{	
 		// 4-directional movement
 		if( Input.getState( "LookUp" ) )
 		{
-			this.transform.position.z -= 0.15 * max( Time.deltaTime.total!"msecs"(), 1 );
+			this.transform.position.z -= moveSpeed * max( Time.deltaTime.total!"msecs"(), 1 );
 		}
 		else if( Input.getState( "LookDown" ) )
 		{
-			this.transform.position.z += 0.15 * max( Time.deltaTime.total!"msecs"(), 1 );
+			this.transform.position.z += moveSpeed * max( Time.deltaTime.total!"msecs"(), 1 );
 		}
 		
 		if( Input.getState( "LookLeft" ) )
 		{
-			this.transform.position.x -= 0.15 * max( Time.deltaTime.total!"msecs"(), 1 );
+			this.transform.position.x -= moveSpeed * max( Time.deltaTime.total!"msecs"(), 1 );
 		}
 		else if( Input.getState( "LookRight" ) )
 		{
-			this.transform.position.x += 0.15 * max( Time.deltaTime.total!"msecs"(), 1 );
+			this.transform.position.x += moveSpeed * max( Time.deltaTime.total!"msecs"(), 1 );
 		}
-
+		
 		// change distance from floor
 		if( Input.getState( "ZoomUp" ) )
 		{
-			this.transform.position.y -= 0.15 * max( Time.deltaTime.total!"msecs"(), 1 );
+			this.transform.position.y -= moveSpeed * max( Time.deltaTime.total!"msecs"(), 1 );
 		}
 		else if( Input.getState( "ZoomDown" ) )
 		{
-			this.transform.position.y += 0.15 * max( Time.deltaTime.total!"msecs"(), 1 );
+			this.transform.position.y += moveSpeed * max( Time.deltaTime.total!"msecs"(), 1 );
 		}
 	}
 }
