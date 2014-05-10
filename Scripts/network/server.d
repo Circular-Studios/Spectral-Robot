51b4ad90ce79334a52a67a81dc4e7fa47cbf20cd
Colden Cullen <ColdenCullen@gmail.com>
2014-05-10 12:57:00 -0400

    Added (basic) second screen (web site) support!

--- Scripts/network/server.d
+++ Scripts/network/server.d
@@ -5,9 +5,13 @@ public import action;
 import speed, speed.db;
 //import vibe.d;
 import core.thread;
-import std.stdio, std.algorithm;
-
+import std.stdio, std.algorithm, std.concurrency;
 import dvorm;
+import vibe.d;
+
+uint gamesPlayed;
+uint numPlayers;
+shared ConnectionManager connMan;
 
 @dbName("turn")
 class Turn
@@ -40,17 +44,31 @@ static this()
 	databaseConnection = DbConnection( DbType.Mongo, "127.0.0.1", ushort.init, null, null, "spectral" );
 }
 
+shared static this()
+{
+	auto settings = new HTTPServerSettings;
+	settings.port = 9090;
+
+	listenHTTP(settings, &handleRequest);
+}
+
+void handleRequest( HTTPServerRequest req, HTTPServerResponse res )
+{
+	writeln( "New http request." );
+	res.writeBody( "Number of games played: " ~ ( gamesPlayed + 1 ).to!string ~ "\nNumber of active players: " ~ connMan.connections.length.to!string ~ ".", "text/plain" );
+}
 
 void main()
 {
-	for( uint gamesPlayed = 0; ; ++gamesPlayed )
+	for( gamesPlayed = 0; ; ++gamesPlayed )
 	{
-		uint numPlayers = 0;
-		auto connMan = ConnectionManager.open();
+		numPlayers = 0;
+		connMan = ConnectionManager.open();
 
 		connMan.onNewConnection ~= ( shared Connection conn )
 		{
 			numPlayers++;
+			writeln( "Now have ", numPlayers, " players." );
 			conn.onReceiveData!string ~= ( string msg )
 			{
 				writeln( "Recieved message: ", msg );
@@ -58,18 +76,16 @@ void main()
 				conn.send!uint( numPlayers );
 				writeln( "numPlayers: ", numPlayers );
 
-				/*
 				if( msg == "ready")
 				{
-					auto turns = Turn.findAll().filter!(turn => turn.gameID == gameID);
+					auto turns = Turn.findAll().filter!(turn => turn.gameID == gamesPlayed);
 					
 					foreach( Turn t; turns )
 					{
 						writeln( "Sending turn: ",  t.toAction);
-						connMan.send!Action( t.toAction, ConnectionType.TCP );
+						conn.send!Action( t.toAction, ConnectionType.TCP );
 					}
 				}
-				*/
 			};
 
 			conn.onReceiveData!Action ~= ( Action action )
@@ -82,25 +98,32 @@ void main()
 						otherConn.send!Action( action, ConnectionType.TCP );
 				}
 
-				/*
 				if( action.saveToDatabase )
 				{
 					auto sesh = new Turn( action, gamesPlayed );
 					sesh.save();
 					writeln( "Saved session. New count: ", Turn.findAll().length );
-
-
-					//Turn.findAll().filter!(turn => turn.gameID == gamesPlayed);
 				}
-				8*/
 			};
 		};
 
 		connMan.start();
 
-		// Wait for all players to disconnect.
-		while( numPlayers != 0 ) { }
+		// Wait for first player to connect.
+		writeln( "Waiting for first player..." );
+		while( connMan.connections.length == 0 )
+		{
+			processEvents();
+		}
 
+		writeln( "Player connected, now waiting for all connections to close." );
+		// Wait for all players to disconnect.
+		while( connMan.connections.length != 0 )
+		{
+			processEvents();
+		}
+
+		writeln( "All connections lost. restarting." );
 		connMan.close();
 		Thread.sleep( 500.msecs );
 	}
