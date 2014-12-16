9de776e593e3ee870fb60dce07b37839371773ae
Daniel Jost <daniel@danieljost.com>
2014-12-15 23:12:59 -0500

    Tabs to spaces in gamemode.d

--- Scripts/gameMode.d
+++ Scripts/gameMode.d
@@ -3,28 +3,35 @@ import dash;
 
 enum GameMode
 {
-	Deathmatch,
-	CTF // capture the flag
+  Deathmatch,
+  CTF // capture the flag
 }
 
 class TurnCounter
 {
 public:
-	int turnCounter;
-	
-	int robotKills = 0;
-	int wolfKills = 0;
+  int turnCounter;
 
-	this (GameMode gameMode) {
-		if (gameMode == GameMode.Deathmatch) {
-			turnCounter = 20;
-		} else {
-			turnCounter = -1;
-		}
-	}
-	
-	void Iterate() {
-		if (turnCounter > 0) turnCounter --;
-		info("Turns Left: ", turnCounter);
-	}
+  int robotKills = 0;
+  int wolfKills = 0;
+
+  this( GameMode gameMode )
+  {
+    if ( gameMode == GameMode.Deathmatch )
+    {
+      turnCounter = 20;
+    }
+    else
+    {
+      turnCounter = -1;
+    }
+  }
+
+  void Iterate()
+  {
+    if( turnCounter > 0 )
+      turnCounter--;
+
+    info( "Turns Left: ", turnCounter );
+  }
 }
